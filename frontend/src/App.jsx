47ae0c890a20b2ac8c03752aefcde7e01fc0a6d8
Hailey Wu <haileyyiyu@gmail.com>
2025-04-29 00:50:38 -0700

    Frontend routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import CreatePet from './pages/CreatePet';
+import DeletePet from './pages/DeletePet';
+import ShowPet from './pages/DeletePet';
+import UpdatePet from './pages/UpdatePet';
+
 
 const App = () => {
   return (
     <Routes>
+      <Route path='/' element = {<Home />} />
+      <Route path='/pets/create' element = {<CreatePet />} />
+      <Route path='/pets/delete/:id' element = {<DeletePet />} />
+      <Route path='/pets/update/:id' element = {<UpdatePet />} />
+      <Route path='/pets/details/:id' element = {<ShowPet />} />
     </Routes>
   )
 }
---
+++ frontend/src/pages/ShowPet.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShowPet = () => {
+  return (
+    <div>ShowPet</div>
+  )
+}
+
+export default ShowPet
