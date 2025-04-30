04d3430d629bef88b5baeb6ac7038bfa441052d5
Hailey Wu <haileyyiyu@gmail.com>
2025-04-30 14:38:44 -0700

    Handled pet creation

--- frontend/src/pages/CreatePet.jsx
+++ frontend/src/pages/CreatePet.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import { useNavigate } from 'react-router-dom';
 
 const CreatePet = () => {
-  return (
-    <div>CreatePet</div>
-  )
+    const [name, setName] = useState('');
+    const [age, setAge] = useState('');
+    const [breed, setBreed] = useState('');
+    const [loading, setLoading] = useState('');
+    const navigate = useNavigate();
+    const handleSavePet = () => {
+        const data = {
+            name,
+            age,
+            breed
+        };
+        setLoading(true);
+        axios.post(`http://localhost:5555/pets`, data)
+            .then(() => {
+                setLoading(false);
+                navigate('/');
+            })
+            .catch((error) => {
+                setLoading(false);
+                console.log(error);
+            })
+    }
+    return (
+        <div>CreatePet</div>
+    )
 }
 
 export default CreatePet
