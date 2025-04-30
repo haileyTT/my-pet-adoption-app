687ef3528ee363a0cf504827cbfd6273322ee865
Hailey Wu <haileyyiyu@gmail.com>
2025-04-29 21:57:34 -0700

    Implmented ShowPet

--- frontend/src/pages/ShowPet.jsx
+++ frontend/src/pages/ShowPet.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
 
 const ShowPet = () => {
-  return (
-    <div>ShowPet</div>
-  )
+    const [pet, setPet] = useState({});
+    const [loading, setLoading] = useState(false);
+    const { id } = useParams();
+
+    useEffect(() => {
+        setLoading(true);
+        axios.get(`http://localhost:5555/pets/${id}`)
+            .then((response) => {
+                setPet(response.data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.log(error);
+                setLoading(false);
+            })
+    }, [id])
+
+    return (
+        <div className='p-4'>
+            <BackButton />
+            <h1 className='text-3xl my-4'>Details</h1>
+            {
+                loading ? (
+                    <Spinner />
+                ) : (
+                    <div className='flex flex-col border-2 p-4 w-fit rounded-xl'>
+                        <div className='my-4'>
+                            <span className='mr-4 text-xl'>Id</span>
+                            <span>{pet._id}</span>
+                        </div>
+                        <div className='my-4'>
+                            <span className='mr-4 text-xl'>Name</span>
+                            <span>{pet.name}</span>
+                        </div>
+                        <div className='my-4'>
+                            <span className='mr-4 text-xl'>Age</span>
+                            <span>{pet.age}</span>
+                        </div>
+                        <div className='my-4'>
+                            <span className='mr-4 text-xl'>Breed</span>
+                            <span>{pet.breed}</span>
+                        </div>
+                        <div className='my-4'>
+                            <span className='mr-4 text-xl'>Create Time</span>
+                            <span>{new Date(pet.createdAt).toLocaleString()}</span>
+                        </div>
+                        <div className='my-4'>
+                            <span className='mr-4 text-xl'>Last Updated</span>
+                            <span>{new Date(pet.updatedAt).toLocaleString()}</span>
+                        </div>
+                    </div>
+                )
+            }
+        </div>
+    )
 }
 
 export default ShowPet
