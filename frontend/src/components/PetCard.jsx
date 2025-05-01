32966a933e7e8f3dbaaf3ef143b68ba918e96cea
Hailey Wu <haileyyiyu@gmail.com>
2025-04-30 18:47:33 -0700

    Pet card styling modified

--- frontend/src/components/PetCard.jsx
+++ frontend/src/components/PetCard.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
 
 const PetCard = ({ pet }) => {
     const { name, imageUrl, breed, age } = pet;
@@ -11,11 +14,22 @@ const PetCard = ({ pet }) => {
                 src={`http://localhost:5555/${imageUrl}`}
                 alt={name}
             />
-            <div className="px-6 py-4">
+            <div className="px-6 py-4 flex items-center justify-between">
                 <div className="font-bold text-xl mb-2">{name}</div>
-                <p className="text-gray-700 text-base">
+                <div className='flex gap-x-4'>
+                    <Link to={`/pets/details/${pet._id}`} >
+                        <BsInfoCircle className='text-xl' />
+                    </Link>
+                    <Link to={`/pets/update/${pet._id}`} >
+                        <AiOutlineEdit className='text-xl' />
+                    </Link>
+                    <Link to={`/pets/delete/${pet._id}`} >
+                        <MdOutlineDelete className='text-xl' />
+                    </Link>
+                </div>
+                {/* <p className="text-gray-700 text-base">
                     Lorem ipsum dolor sit amet.
-                </p>
+                </p> */}
             </div>
             <div className="px-6 pt-4 pb-2">
                 <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{breed}</span>
